606dca7a3c91428472bca774b0969a7c54cecf4b
nicknovick <nnnovick@gmail.com>
2021-03-02 23:05:54 +0100

    Added leddimmen02.py

---
+++ leddimmen02.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+LED = [17, 22]
+
+GPIO.setup(LED[0], GPIO.OUT)
+GPIO.setup(LED[1], GPIO.OUT)
+print("Strg+C beendet das Programm")
+p = GPIO.PWM(LED[0], 50)
+q = GPIO.PWM(LED[1], 50)
+p.start(0)
+q.start(0)
+try:
+    while True:
+        for c in range(0, 101, 2):
+            p.ChangeDutyCycle(c)
+            q.ChangeDutyCycle(c)
+            time.sleep(0.1)
+        q.ChangeFrequency(10)
+        for c in range(0, 101, 2):
+            p.ChangeDutyCycle(c)
+            q.ChangeDutyCycle(100-c)
+            time.sleep(0.1)
+        q.ChangeFrequency(50)
+except KeyboardInterrupt:
+    p.stop()
+    q.stop()
+    GPIO.cleanup()
